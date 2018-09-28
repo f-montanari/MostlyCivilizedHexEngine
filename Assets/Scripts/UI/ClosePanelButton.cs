1326b1508ea67bf88b4a5f7c67852daf8a71d334
Franco Montanari <3f.montanari@gmail.com>
2018-09-28 15:05:20 -0300

    Close button added to City panel

---
+++ Assets/Scripts/UI/ClosePanelButton.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class ClosePanelButton : MonoBehaviour
+{
+    public RectTransform Panel;
+    public void ClosePanel()
+    {
+        if(Panel == null)
+        {
+            Debug.LogError("No panel has been set");
+            return;
+        }
+        Panel.gameObject.SetActive(false);
+    }
+}
